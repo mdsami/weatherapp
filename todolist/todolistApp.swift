67c1920a29c7ddf71798b82f37dfb72d889f6509
MD SAMI <mdsami.diu@gmail.com>
2021-04-10 12:21:56 +0600

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by mdsami on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
